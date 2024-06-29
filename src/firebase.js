8cfadd03eaeffe2640ef18c603003a0da09c673f
Jonathan Silva <jonathandearaujosilva@gmail.com>
2024-06-29 15:16:00 -0300

    r conf

--- src/firebase.js
+++ src/firebase.js
@@ -3,14 +3,7 @@ import { getDatabase } from 'firebase/database'
 
 // TODO: Replace the following with your app's Firebase project configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyAJePLDvL99XimEub8qfKWUMJ1KVILT-jY",
-  authDomain: "aula-cesar-intero.firebaseapp.com",
-  databaseURL: "https://aula-cesar-intero-default-rtdb.firebaseio.com",
-  projectId: "aula-cesar-intero",
-  storageBucket: "aula-cesar-intero.appspot.com",
-  messagingSenderId: "394089619495",
-  appId: "1:394089619495:web:de2560de6b2bf0cd50ee02",
-  measurementId: "G-KKR0YD58H0"
+
 };
 
 const app = initializeApp(firebaseConfig);
