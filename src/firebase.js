d9fa377dfd867164f2a64b94e47ccd1d44e4d6ee
Jonathan Silva <jonathandearaujosilva@gmail.com>
2024-06-29 18:09:56 -0300

    r conf

--- src/firebase.js
+++ src/firebase.js
@@ -3,7 +3,14 @@ import { getDatabase } from 'firebase/database'
 
 // TODO: Replace the following with your app's Firebase project configuration
 const firebaseConfig = {
-
+  apiKey: import.meta.env.VITE_apiKey,
+  authDomain: import.meta.env.VITE_authDomain,
+  databaseURL: import.meta.env.VITE_databaseURL,
+  projectId: import.meta.env.VITE_projectId,
+  storageBucket: import.meta.env.VITE_storageBucket,
+  messagingSenderId: import.meta.env.VITE_messagingSenderId,
+  appId: import.meta.env.VITE_appId,
+  measurementId: import.meta.env.VITE_measurementId,
 };
 
 const app = initializeApp(firebaseConfig);
